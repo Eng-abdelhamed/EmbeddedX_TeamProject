95129b32b0768fdaeaa5e7659a440ffba4622069
Wagdy Mohamed <wagdymohamed1411@gmail.com>
2025-09-14 13:43:06 +0300

    Delete Bridge_ECU directory

--- Bridge_ECU/Debug/MCAL/uart.d
+++
@@ -1,8 +0,0 @@
-MCAL/uart.o MCAL/uart.o: ../MCAL/uart.c ../MCAL/uart.h \
-  ../MCAL/../common/stdtypes.h ../MCAL/../common/common_macros.h
-
-../MCAL/uart.h:
-
-../MCAL/../common/stdtypes.h:
-
-../MCAL/../common/common_macros.h:
